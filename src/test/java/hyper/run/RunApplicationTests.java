5312dc1f3fd78025f76a661e2cbc1da588752d88
sinminseok12@naver.com <55183314+sinminseok@users.noreply.github.com>
2025-07-15 16:28:43 +0900

    Init commit

---
+++ src/test/java/hyper/run/RunApplicationTests.java
@@ -0,0 +1,13 @@
+package hyper.run;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
