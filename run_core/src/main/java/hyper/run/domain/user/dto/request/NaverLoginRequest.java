6952ed1a6e7a8125495728386cdb05515be17c22
sinminseok12@naver.com <55183314+sinminseok@users.noreply.github.com>
2025-08-11 17:30:44 +0900

    feat : Google, Naver, Kakao 소셜로그인 기능 구현

---
+++ run_core/src/main/java/hyper/run/domain/user/dto/request/NaverLoginRequest.java
@@ -0,0 +1,15 @@
+package hyper.run.domain.user.dto.request;
+
+
+
+public class NaverLoginRequest {
+    private String accessToken;
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+}
