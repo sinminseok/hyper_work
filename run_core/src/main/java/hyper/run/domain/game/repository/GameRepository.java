b280b1ea1bbb59f7af36c4024ad2e885964f354b
brian <brian506@naver.com>
2025-08-16 13:01:24 +0900

    feat : 결제 관리 구현

--- run_core/src/main/java/hyper/run/domain/game/repository/GameRepository.java
+++ run_core/src/main/java/hyper/run/domain/game/repository/GameRepository.java
@@ -1,9 +1,6 @@
 package hyper.run.domain.game.repository;
 
 import hyper.run.domain.game.entity.Game;
-import hyper.run.domain.game.entity.GameStatus;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
