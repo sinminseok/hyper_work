1b8ac45dc6498bada76a21a01d78ae8b32bda5d4
brian <brian506@naver.com>
2025-11-23 18:57:42 +0900

    refactor : jenkins 배포 진행중

--- run_core/src/main/java/hyper/run/domain/game/entity/GameType.java
+++ run_core/src/main/java/hyper/run/domain/game/entity/GameType.java
@@ -5,9 +5,9 @@ public enum GameType {
     HEARTBEAT("심박수"),
     VERTICAL_OSCILLATION("수직 진폭"),
     GROUND_CONTACT_TIME("지면 접촉 시간"),
-    POWER("강성"),
-    CADENCE("케이던스"),
-    FLIGHT_TIME("체공시간");
+    //    POWER("강성"),
+    CADENCE("케이던스");
+//    FLIGHT_TIME("체공시간"),
 
     private final String name;
 
