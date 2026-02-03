d35c7d7f63a6eb66ca3d23beffbaf20082986864
sinminseok12@naver.com <55183314+sinminseok@users.noreply.github.com>
2026-02-04 05:47:15 +0900

    feat : 결제 장애 수정

--- run_core/src/main/java/hyper/run/common/message/SqsMessage.java
+++ run_core/src/main/java/hyper/run/common/message/SqsMessage.java
@@ -1,8 +1,10 @@
 package hyper.run.common.message;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import hyper.run.common.enums.JobType;
 
 
 public interface SqsMessage {
+    @JsonIgnore
     JobType getJobType();
 }
